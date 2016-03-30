bee0d1bd9831a475ae24c58394f2ff7a3117c1fc
NaotoKishino <KishinoNaoto@next-group.jp>
2016-03-30 09:36:23 +0000

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,3 @@
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/inputrc ~/.inputrc
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/jshintrc ~/.jshintrc
-ls -ltra
-
 cd ~/dotfiles/vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
 
@@ -17,3 +9,13 @@ git submodule update
 git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.br branch
+
+cd ~
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/inputrc ~/.inputrc
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/jshintrc ~/.jshintrc
+ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
+ls -ltra
